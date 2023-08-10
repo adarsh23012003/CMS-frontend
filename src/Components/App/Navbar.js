c02a315244aa1d0c2bf615e1e1388b103c5a00f5
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-08-10 20:18:53 +0530

    footer and navbar added

---
+++ src/Components/App/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <div>
+        <nav className='p-2 bg-blue-400'>
+          <ul className='flex gap-2 font-serif justify-center'>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
