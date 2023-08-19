a3bba6bd581f3b7a3473a656b2724a80e46250de
Shani <shanivish95@gmail.com>
2023-08-19 11:41:43 +0530

    some css changes

--- src/Components/App/Login.js
+++ src/Components/App/Login.js
@@ -38,7 +38,7 @@ const Login = () => {
 
   return (
     <div>
-      <section className=''>
+      <section className='bg-gray-300'>
         <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
           <a
             href='#'
@@ -46,7 +46,7 @@ const Login = () => {
           >
             Login
           </a>
-          <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0  dark:border-gray-700'>
+          <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0  border-sky-400'>
             <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
               <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl '>
                 Sign in to your account
