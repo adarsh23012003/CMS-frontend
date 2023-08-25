00213f27b8f7eb6c91c49c02108314cd7a591ca7
Shani <shanivish95@gmail.com>
2023-08-25 13:10:06 +0530

    navbar in some changes

--- src/Components/App/Navbar.js
+++ src/Components/App/Navbar.js
@@ -1,7 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { deleteCookie, getCookie } from '../../Axios/cookieConfig'
 
 function Navbar() {
+  const [isLogin, setIsLogin] = useState(false)
+  const login = getCookie('token')
+  console.log(isLogin)
+  const logOut = () => {
+    console.log('call function')
+    deleteCookie('token')
+    setIsLogin(false)
+    console.log(isLogin)
+  }
+  useEffect(() => {
+    if (!login) {
+      setIsLogin(true)
+    }
+  })
   return (
     <>
       <div>
@@ -10,11 +25,20 @@ function Navbar() {
             <li>
               <Link to='/'>Home</Link>
             </li>
+            {!isLogin ? (
+              <li>
+                <Link to='/login'>Login</Link>
+              </li>
+            ) : (
+              <li>
+                <Link onClick={logOut}>LogOut</Link>
+              </li>
+            )}
           </ul>
         </nav>
       </div>
     </>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
